18e6cceff88c5b8b99d028e5aa115d3f80f102ee
San4255 <sansaxena02@gmail.com>
2022-10-26 12:40:29 +0000

    pex bringup

--- aosp_phoenix.mk
+++ aosp_phoenix.mk
@@ -7,19 +7,14 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := aosp_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
 PRODUCT_MANUFACTURER := Xiaomi
-RICE_MAINTAINER := Risan (@Risan4255)
-TARGET_BUILD_GRAPHENEOS_CAMERA := ture
-TARGET_OPTOUT_GOOGLE_TELEPHONY := true
-RICE_PACKAGE_TYPE :=  Gapps
-TARGET_USE_PIXEL_FINGERPRINT := true
 
 # PixelExperience Properties
 TARGET_GAPPS_ARCH := arm64
@@ -29,3 +24,5 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR := false
 WITH_GMS := true
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PEX_BUILD_TYPE := UNOFFICIAL
+PEX_MAINTAINER := Risan
