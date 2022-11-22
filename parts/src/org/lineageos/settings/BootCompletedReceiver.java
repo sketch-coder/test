65dc7bae3010200b31eb75f445fb1dff28728999
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2022-11-23 01:18:47 +0530

    phoenix: parts: import per app refresh rate

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,6 +26,7 @@ import android.provider.Settings;
 import org.lineageos.settings.soundcontrol.SoundControlSettings;
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.smartcharging.SmartCharging;
+import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -51,6 +52,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             )
         );
         new SmartCharging(context).onBootCompleted();
+        RefreshUtils.startService(context);
 
         // === DELAYED TASKS === //
 
